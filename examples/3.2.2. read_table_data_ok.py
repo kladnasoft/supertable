adf8a5e10c4664705c58b4f98275320acb6debd8
kladnasoft <kladnasoft@outlook.com>
2025-10-17 05:53:14 +0200

    rework: data reader split

--- examples/3.2.2. read_table_data_ok.py
+++ examples/3.2.2. read_table_data_ok.py
@@ -1,10 +1,10 @@
-from supertable.data_reader import DataReader
+from supertable.data_reader import DataReader, engine
 from examples.defaults import super_name, user_hash, simple_name, organization
 
 query = f"select * as cnt from {simple_name} where 1=1 limit 10"
 
 dr = DataReader(super_name=super_name, organization=organization, query=query)
-result = dr.execute(user_hash=user_hash, with_scan=False)
+result = dr.execute(user_hash=user_hash, with_scan=False, engine=engine.DUCKDB)
 print("-" * 52)
 print("Rows: ", result[0].shape[0], ", Columns: ", result[0].shape[1], ", " , result[1], ", Message: ", result[2])
 print("-" * 52)
